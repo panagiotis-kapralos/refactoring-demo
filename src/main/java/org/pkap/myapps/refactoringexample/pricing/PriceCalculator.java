067320aa9ebe02a9b39667148e8cc455831d2d4a
Panagiotis Kapralos <panagiotis.kapralos@trasysgroup.com>
2017-01-27 16:35:36 +0200

    Refactoring 1: Extract method calculatePriceWithVat

--- src/main/java/org/pkap/myapps/refactoringexample/pricing/PriceCalculator.java
+++ src/main/java/org/pkap/myapps/refactoringexample/pricing/PriceCalculator.java
@@ -14,18 +14,23 @@ public class PriceCalculator {
     public BigDecimal calculatePrice(Order order, String countryCode, BigDecimal discountRate) {
         BigDecimal totalPrice = new BigDecimal("0.00");
         for (Product product : order.getProducts()) {
-            BigDecimal productPrice = priceList.getPriceFor(product);
-            if ("GR".equals(countryCode)) {
-                productPrice = productPrice.add(productPrice.multiply(new BigDecimal("0.24")).setScale(2, RoundingMode.HALF_UP));
-            } else if ("DE".equals(countryCode)) {
-                productPrice = productPrice.add(productPrice.multiply(new BigDecimal("0.19")).setScale(2, RoundingMode.HALF_UP));
-            } else if ("FR".equals(countryCode)) {
-                productPrice = productPrice.add(productPrice.multiply(new BigDecimal("0.20")).setScale(2, RoundingMode.HALF_UP));
-            }
+            BigDecimal productPrice = calculatePriceWithVat(product, countryCode);
             totalPrice = totalPrice.add(productPrice);
         }
         totalPrice = totalPrice.subtract(totalPrice.multiply(discountRate).setScale(2, RoundingMode.HALF_UP));
         return totalPrice;
+    }
+
+    private BigDecimal calculatePriceWithVat(Product product, String countryCode) {
+        BigDecimal productPrice = priceList.getPriceFor(product);
+        if ("GR".equals(countryCode)) {
+            productPrice = productPrice.add(productPrice.multiply(new BigDecimal("0.24")).setScale(2, RoundingMode.HALF_UP));
+        } else if ("DE".equals(countryCode)) {
+            productPrice = productPrice.add(productPrice.multiply(new BigDecimal("0.19")).setScale(2, RoundingMode.HALF_UP));
+        } else if ("FR".equals(countryCode)) {
+            productPrice = productPrice.add(productPrice.multiply(new BigDecimal("0.20")).setScale(2, RoundingMode.HALF_UP));
+        }
+        return productPrice;
     }
 
 }
